6c94a8e81ff33eea5ea92841a49f15966a8d8684
RipNasty <det625@gmail.com>
2024-09-26 17:55:04 -0400

    data ingestion

---
+++ scripts/data_ingestion.py
@@ -0,0 +1,27 @@
+import requests
+from scripts.config import API_KEY, BASE_URL
+
+def get_movies_by_year(year):
+    """
+    Function to retrieve movies from the TMDb API by a specific year.
+    """
+    url = f"{BASE_URL}/discover/movie"
+    params = {
+        "api_key": API_KEY,
+        "primary_release_year": year,
+        "sort_by": "popularity.desc",
+    }
+    
+    response = requests.get(url, params=params)
+    
+    if response.status_code == 200:
+        return response.json()  # Return the raw JSON response
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+# Example usage
+if __name__ == "__main__":
+    movies = get_movies_by_year(2023)
+    if movies:
+        print(movies['results'])
